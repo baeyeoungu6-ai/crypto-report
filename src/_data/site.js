7b52472986c0b71a5779c21df3bac8c359183491
baeyeoungu6-ai <baeyeoungu6@gmail.com>
2026-04-30 21:51:36 +0900

    Add files via upload

--- src/_data/site.js
+++ src/_data/site.js
@@ -4,5 +4,7 @@ module.exports = {
   url: "https://alphacoinreport.com",
   binanceReferralUrl: "https://www.binance.com/join?ref=XETYHXNR",
   binanceReferralCode: "XETYHXNR",
-  binanceBannerImage: "/assets/binance-event-link.png"
+  binanceBannerImage: "/assets/binance-event-link.webp",
+  okxReferralUrl: "https://okx.com/join/9603836",
+  okxBannerImage: "/assets/okx-event-link.webp"
 };
